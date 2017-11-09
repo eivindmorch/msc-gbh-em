f10d11f2a1b09337b88c4cdde87f0ceb3d3944a6
Eivind Morch <eivinmor@stud.ntnu.no>
2017-11-09 14:03:17 +0100

    Added gitignore and changed data path

--- src/main/java/util/Values.java
+++ src/main/java/util/Values.java
@@ -4,8 +4,8 @@ package util;
 public class Values {
 
     public static String filePathRoot = System.getProperty("user.dir") + "/src/main/resources/";
-    public static String processedDataPath = "processed/";
-    public static String rawDataPath = "raw/";
+    public static String processedDataPath = "data/processed/";
+    public static String rawDataPath = "data/raw/";
 
     public static String rawDataHeader = "timestamp, posX, posY, posZ, velX, velY, velZ";
     public static String processedDataHeader = "timestamp, distance, angle";
