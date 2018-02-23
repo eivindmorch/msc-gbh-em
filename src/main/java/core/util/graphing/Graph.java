6309b64458f011fca9c33ad9405c97ca0a4ed6b6
Eivind Morch <eivinmor@stud.ntnu.no>
2018-02-23 18:07:48 +0100

    Moved all graph styling to Style

--- src/main/java/core/util/graphing/Graph.java
+++ src/main/java/core/util/graphing/Graph.java
@@ -1,22 +1,10 @@
 package core.util.graphing;
 
-import com.mxgraph.layout.mxCompactTreeLayout;
-import com.mxgraph.model.mxCell;
-import com.mxgraph.model.mxGeometry;
 import com.mxgraph.swing.mxGraphComponent;
-import com.mxgraph.util.mxConstants;
-import com.mxgraph.util.mxRectangle;
 import com.mxgraph.view.mxGraph;
-import com.mxgraph.view.mxPerimeter;
-import com.mxgraph.view.mxStylesheet;
-import core.BtreeAlt.CompositeTasks.TempCompositeTask;
-import core.BtreeAlt.LeafTasks.TempConditionTask;
-import core.BtreeAlt.LeafTasks.TempLeafTask;
 import core.BtreeAlt.TempTask;
 
 import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.Map;
 
 class  Graph {
 
@@ -47,32 +35,9 @@ class  Graph {
         } finally {
             mxGraph.getModel().endUpdate();
         }
-        resizeVertices();
-        fixLayout();
+        Style.resizeVertices(mxGraph, vertices);
+        Style.fixLayout(mxGraph, graphParent);
         return new mxGraphComponent(mxGraph);
-    }
-
-    private void resizeVertices() {
-        mxGraph.getModel().beginUpdate();
-        try {
-            // Resize cells
-            for (Object vertex : vertices) {
-                mxCell cell = (mxCell) vertex;
-                mxGeometry g = (mxGeometry) cell.getGeometry().clone();
-                mxRectangle bounds = mxGraph.getView().getState(cell).getLabelBounds();
-                g.setHeight(bounds.getHeight() + 10);
-                g.setWidth(bounds.getWidth() + 20);
-                mxGraph.cellsResized(new Object[]{cell}, new mxRectangle[]{g});
-            }
-        } finally {
-            mxGraph.getModel().endUpdate();
-        }
-    }
-
-    private void fixLayout() {
-        mxCompactTreeLayout treeLayout = new mxCompactTreeLayout(mxGraph);
-        treeLayout.setHorizontal(false);
-        treeLayout.execute(graphParent);
     }
 
     mxGraphComponent getComponent() {
@@ -91,16 +56,7 @@ class  Graph {
 
     private Object addVertex(TempTask tempTask) {
         String name = tempTask.getDisplayName();
-
-        String style = null;
-        if (tempTask instanceof TempCompositeTask) {
-            style = "COMPOSITE";
-        } else if (tempTask instanceof TempConditionTask) {
-            style = "CONDITION";
-        } else if (tempTask instanceof TempLeafTask) {
-            style = "ACTION";
-        }
-
+        String style = Style.getCellStyle(tempTask);
         Object vertex = mxGraph.insertVertex(graphParent, null, name, 0, 0, 0, 0, style);
         vertices.add(vertex);
         return vertex;
--- src/main/java/core/util/graphing/Style.java
+++ src/main/java/core/util/graphing/Style.java
@@ -1,18 +1,32 @@
 package core.util.graphing;
 
+import com.mxgraph.layout.mxCompactTreeLayout;
+import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxGeometry;
 import com.mxgraph.util.mxConstants;
+import com.mxgraph.util.mxRectangle;
+import com.mxgraph.view.mxGraph;
 import com.mxgraph.view.mxPerimeter;
 import com.mxgraph.view.mxStylesheet;
 import com.sun.javaws.exceptions.InvalidArgumentException;
+import core.BtreeAlt.CompositeTasks.TempCompositeTask;
+import core.BtreeAlt.LeafTasks.TempConditionTask;
+import core.BtreeAlt.LeafTasks.TempLeafTask;
+import core.BtreeAlt.TempTask;
 import core.model.btree.BehaviorTreeUtil;
 import experiments.experiment1.unit.Experiment1UnitInfo;
 import experiments.experiment1.unit.FollowerUnit;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 
 abstract class Style {
 
     private static mxStylesheet styleSheet = createStyleSheet();
+
+    private static final String COMPOSITE = "COMPOSITE";
+    private static final String CONDITION = "CONDITION";
+    private static final String ACTION = "ACTION";
 
     static mxStylesheet getStyleSheet() {
         return styleSheet;
@@ -20,9 +34,9 @@ abstract class Style {
 
     private static mxStylesheet createStyleSheet() {
         mxStylesheet stylesheet = new mxStylesheet();
-        stylesheet.putCellStyle("COMPOSITE", createCompositeStyle());
-        stylesheet.putCellStyle("ACTION", createActionStyle());
-        stylesheet.putCellStyle("CONDITION", createConditionStyle());
+        stylesheet.putCellStyle(COMPOSITE, createCompositeStyle());
+        stylesheet.putCellStyle(CONDITION, createConditionStyle());
+        stylesheet.putCellStyle(ACTION, createActionStyle());
         stylesheet.setDefaultEdgeStyle(createEdgeStyle());
         return stylesheet;
     }
@@ -93,10 +107,48 @@ abstract class Style {
         return style;
     }
 
+    static String getCellStyle(TempTask tempTask) {
+        String style = null;
+        if (tempTask instanceof TempCompositeTask) {
+            style = COMPOSITE;
+        } else if (tempTask instanceof TempConditionTask) {
+            style = CONDITION;
+        } else if (tempTask instanceof TempLeafTask) {
+            style = ACTION;
+        }
+        return style;
+    }
+
+    static void resizeVertices(mxGraph mxGraph, ArrayList<Object> vertices) {
+        mxGraph.getModel().beginUpdate();
+        try {
+            for (Object vertex : vertices) {
+                mxCell cell = (mxCell) vertex;
+                mxGeometry g = (mxGeometry) cell.getGeometry().clone();
+                mxRectangle bounds = mxGraph.getView().getState(cell).getLabelBounds();
+                g.setHeight(bounds.getHeight() + 15);
+                g.setWidth(bounds.getWidth() + 20);
+                mxGraph.cellsResized(new Object[]{cell}, new mxRectangle[]{g});
+            }
+        } finally {
+            mxGraph.getModel().endUpdate();
+        }
+    }
+
+    static void fixLayout(mxGraph mxGraph, Object graphParent) {
+        mxCompactTreeLayout treeLayout = new mxCompactTreeLayout(mxGraph);
+
+        treeLayout.setHorizontal(false);
+        treeLayout.setEdgeRouting(true);
+        treeLayout.setLevelDistance(40);
+
+        treeLayout.execute(graphParent);
+    }
+
     public static void main(String[] args) {
         Experiment1UnitInfo.init();
         try {
-            Grapher.quickGraph("asd", BehaviorTreeUtil.generateRandomTree(FollowerUnit.class, 5, 10));
+            Grapher.quickGraph("asd", BehaviorTreeUtil.generateRandomTree(FollowerUnit.class, 15, 30));
         } catch (InvalidArgumentException e) {
             e.printStackTrace();
         }
