7115446e826cebb6ff248d74d78c1cb3b3361cfc
Eivind Morch <eivinmor@stud.ntnu.no>
2018-04-28 18:48:49 +0200

    Made SimSettings fields final

--- src/main/java/core/simulation/SimSettings.java
+++ src/main/java/core/simulation/SimSettings.java
@@ -4,13 +4,13 @@ public class SimSettings {
 
 
     // -- SETUP ---------------------------------------------------------------------
-        public static String vrfDirectory = System.getenv("MAK_VRFDIR64");
-        public static int siteId = 1;
-        public static String federationName = "rlo";
-        public static String federationFile = "RPR_FOM_v2.0_1516-2010.xml";
-        public static String rprFomVersion = "2.0";
+        public static final String vrfDirectory = System.getenv("MAK_VRFDIR64");
+        public static final int siteId = 1;
+        public static final String federationName = "rlo";
+        public static final String federationFile = "RPR_FOM_v2.0_1516-2010.xml";
+        public static final String rprFomVersion = "2.0";
 
-        public static String[] fomModules = new String[] {
+        public static final String[] fomModules = new String[] {
                 "MAK-VRFExt-1_evolved.xml",
                 "MAK-DIGuy-2_evolved.xml",
                 "MAK-LgrControl-1_evolved.xml",
@@ -22,18 +22,18 @@ public class SimSettings {
 
 
         // SimEngine
-        public static int simEngineApplicationNumber = 3001;
-        public static boolean useTimeManagement = true;
-        public static boolean startInRunMode = false;
+        public static final int simEngineApplicationNumber = 3001;
+        public static final boolean useTimeManagement = true;
+        public static final boolean startInRunMode = false;
 
-        public static String[] plugins = new String[] {
+        public static final String[] plugins = new String[] {
                 "LLBMLSimHLA1516e_VC10.dll",
                 "VrfCgfControl.dll"
         };
 
 
         // SimGUI
-        public static int simGUIApplicationNumber = 3101;
+        public static final int simGUIApplicationNumber = 3101;
 
 
         // RTI
